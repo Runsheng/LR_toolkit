44e4019614a51c3c47bae2dda7ed6dcf316cff9c
Runsheng <runsheng.lee@gmail.com>
2015-04-01 11:03:54 +0800

    add some new function
    such as write fasta, select N

--- temp.py
+++ temp.py
@@ -8,3 +8,25 @@ for read in samfile.fetch(): #the fetch is like to choose a subset of the bam fi
 
 samfile.close()
 
+
+# to get per base coverage at a place
+# the str of the pileup function
+# Class: AlignmentFile.pileup.pileups
+# Classname: AlignmentFile.pileup==pileupcolumn; AlignmentFile.pileup.pileups==pileupread
+# Note: the N_list do not include the end of the gap region, so is needed to be expanded
+
+#todo: change the marix in this one
+import pysam
+print N_list[190]
+chro, start,end =N_list[190]
+samfile = pysam.AlignmentFile("cb12i_s.bam", "rb")
+print chr_select(record_dict, chro,start,end+1)
+
+for pileupcolumn in samfile.pileup(chro,start,end+1, truncate=True):
+    print ("\ncoverage at base %s = %s" %
+            (pileupcolumn.pos, pileupcolumn.n))
+    for pileupread in pileupcolumn.pileups:
+        print pileupread.alignment.query_sequence[pileupread.query_position]
+        print pileupread.is_del  #if this position is a deletion,then the replacemnet can be ignored
+        print pileupread.indel
+samfile.close()
