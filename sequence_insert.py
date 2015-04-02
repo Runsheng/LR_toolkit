dbfe4852440923fdb3e22c3e55288feeb3ce86d6
Runsheng <runsheng.lee@gmail.com>
2015-04-02 18:29:54 +0800

    add some new function
    such as write fasta, select N

--- sequence_insert.py
+++ sequence_insert.py
@@ -4,8 +4,7 @@
 # require Biopython:SeqIO to parse the fasta file
 # runsheng 2014/12/15
 # insert new strings into old strings, make a list to store the number of the sequence 
-from utils import reftodic
-
+from utils import fasta2dic
 
 def read_insertion(filename):
     """
@@ -27,24 +26,26 @@ def read_insertion(filename):
 
 # usage todo: need to be modified to be main function
 insertion=read_insertion("insertion_filled.txt")
-record_dict=reftodic("cb4_nfilled.fasta")
+record_dict=fasta2dic("cb4_nfilled.fasta")
 
 def write_insertion(outfile="inserted.fasta"):
     f=open(outfile)
     for name in record_dict.keys():
-        subinsertion={}  # note , a dict is not really needed, a list can do the job
+        subinsertion={}
         print name
         for key in insertion.keys():
             if name==key.split(":")[0]:
-                start_key=int(key.split(":")[1].split("^")[0]) # use number as index, perform better in sorting
-                subinsertion[start_key]=insertion[key]
-        keys_in_order=sorted(subinsertion.keys())
-        seq=list(str(record_dict[name].seq))
-        i=0 # i count for how many insertions were added
-        for key in keys_in_order:
-            seq.insert((key-1+i),subinsertion[key])
-            i+=1
+                subinsertion[key]=insertion[key]
+        seq_old=str(record_dict[name].seq)
+        seq_new=[]
+        for i in range(0,len(seq_old)):
+            for key in subinsertion.keys():
+                if i==(int(key.split("^")[1])-2):
+                    seq_new.append(subinsertion[key])
+            seq_new.append(seq_old[i])
+        print "old", len(seq_old)
+        print "new", len(seq_new)
         f.write(">"+str(name)+"\n")
-        f.write("".join(seq))
+        f.write("".join(seq_new))
         f.write("\n")
     f.close()
