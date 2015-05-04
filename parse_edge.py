47636948fb2a779f464eacd32100ee8108e8c500
Runsheng <runsheng.lee@gmail.com>
2015-05-04 17:03:24 +0800

    change the parse_edge into function

--- parse_edge.py
+++ parse_edge.py
@@ -1,37 +1,36 @@
-f=open("edge.fasta","w")
 
-for ref in ref_length.keys():
-    clipping_l=[]
-    clipping_r=[]
-    for read in samfile.fetch(ref, 0, 100):
-        #cigartuples, 4 is softclipping, 5 is hardclipping, these sequence can extend the end of chrs
-        if read.cigartuples[0][0]==4 or read.cigartuples[0][0]==5:
-            clipping_l.append(read.seq[0:int(read.cigartuples[0][1])])
-    for read in samfile.fetch(ref, (ref_length[ref]-100),ref_length[ref]):
-        #print read.cigartuples
-        if read.cigartuples[-1][0]==4 or read.cigartuples[-1][0]==5:
-            clipping_r.append(read.seq[0:int(read.cigartuples[0][1])])
+import pysam
 
-    try:
-        max_get(clipping_l) # to avoid the output of empty sequence name line
-        #print (">"+ref+"_0^0_left")
-        f.write(max_get(clipping_l))
-        f.write("\n")
-        #print max_get(clipping_l)
-        f.write(">"+ref+"_0^0_left")
-        f.write("\n")
-    except Exception:
-        pass
-        #print "No left clipping signal found."
-    try:
-        max_get(clipping_r)
-        #print (">"+ref+"_"+str(ref_length[ref])+"^"+str(ref_length[ref])+"_right")
-        #print max_get(clipping_r)
-        f.write(">"+ref+"_"+str(ref_length[ref])+"^"+str(ref_length[ref])+"_right")
-        f.write("\n")
-        f.write(max_get(clipping_r))
-        f.write("\n")
-    except Exception:
-        pass
-        #print "No right clipping signal found."
-f.close()
+# samfile=pysam.AlignmentFile("cb12nin_s.bam", "rb")
+
+def edge_parse(samfile):
+    """
+    Input: the samfile is a pysam.Alignmentfile object
+    Output: the bp_list liked tuple-list
+    """
+    ref_length=dict(zip(samfile.references,samfile.lengths))
+
+    for ref in ref_length.keys():
+        clipping_l=[]
+        clipping_r=[]
+        for read in samfile.fetch(ref, 0, 100):
+            # cigartuples, 4 is softclipping, 5 is hardclipping,
+            # TODO: the hard clipping has been ignored, can only be recovered when the fastq file directly
+            if read.cigartuples[0][0]==4:
+                clipping_l.append(read.seq[0:int(read.cigartuples[0][1])])
+        for read in samfile.fetch(ref, (ref_length[ref]-100),ref_length[ref]):
+            if read.cigartuples[-1][0]==4:
+                clipping_r.append(read.seq[0:int(read.cigartuples[0][1])])
+        try:
+            max_get(clipping_l) # to avoid the output of empty sequence name line
+        except Exception as e:
+            print e
+            print "No left clipping signal found in chr %s ." % ref
+        try:
+            max_get(clipping_r)
+        except Exception as e:
+            print e
+            print "No right clipping signal found in chr %s ." % ref
+
+if __name__=="__main__":
+    samfile=pysam.AlignmentFile("cb12nin_s.bam", "rb")
