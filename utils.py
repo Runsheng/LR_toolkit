57c161460949db845034b6487412ad991b5236f2
Runsheng <runsheng.lee@gmail.com>
2015-05-04 09:41:07 +0800

    add some function

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def fastq2dic(fastqfile):
     Give a fastq file name, return a dict contains the name and seq
     Require Biopython SeqIO medule to parse the sequence into dict, a large readfile may take a lot of RAM
     """
-    handle = open(fastafile, "rU")
+    handle = open(fastqfile, "rU")
     record_dict = SeqIO.to_dict(SeqIO.parse(handle, "fastq"))
     handle.close()
     return record_dict
@@ -19,7 +19,7 @@ def fasta2dic(fastafile):
     Require Biopython SeqIO medule to parse the sequence into dict, a large genome may take a lot of RAM
     """
     handle = open(fastafile, "rU")
-    record_dict = SeqIO.to_dict(SeqIO.parse(handle, "fasta"))
+    record_dict = SeqIO.to_dict(SeqIO.parse(handle,"fasta"))
     handle.close()
     return record_dict
 
@@ -34,6 +34,16 @@ def chr_select(record_dict, chr, start,end):
     seq=str(record_dict[chr].seq)[start:end]
     return name,seq
 
+def dic2fasta(record_dict,out="record_dict.fasta"):
+    with open(out,"w") as f:
+        for record in record_dict.keys():
+            name=record
+            seq=record_dict[name]
+            f.write(">")
+            f.write(name)
+            f.write("\n")
+            f.write(seq)
+            f.write("\n")
 
 import subprocess, signal, logging, os
 
@@ -52,7 +62,7 @@ def exe(cmd, timeout=-1):
                             preexec_fn=os.setsid)
     signal.signal(signal.SIGALRM, alarm_handler)
     if timeout > 0:
-        signal.alarm(int(timeout*60))
+        signal.alarm(int(timeout))
     try:
         stdoutVal, stderrVal =  proc.communicate()
         signal.alarm(0)  # reset the alarm
@@ -64,3 +74,13 @@ def exe(cmd, timeout=-1):
         return 214,None,None
     retCode = proc.returncode
     return retCode,stdoutVal,stderrVal
+
+def bwaindex(fastafile):
+    pass
+
+def bwamem():
+    cmd="bwa mem"
+
+### unit test code ###
+if __name__=="__main__":
+    print "aa"
