2ff12858a64c0a2d75dc97f3a9524e227c6405a3
Runsheng <runsheng.lee@gmail.com>
2015-07-14 18:06:28 +0800

    add new ssw function

---
+++ readme.md
@@ -0,0 +1,1 @@
+# The package for long read re-sequence modification.
--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 from utils import exe
 
 print exe("ls -l")
+
+from src.ssw_wrap import Aligner
+
+Aligner.align("AAAAAAA","TTTTTTT")
