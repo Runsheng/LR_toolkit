2625389e22a5bcb0cf079a67411ca86da7a898c4
Runsheng <runsheng.lee@gmail.com>
2016-02-29 14:05:08 +0800

    add gzip support

--- utils.py
+++ utils.py
@@ -1,17 +1,26 @@
 # filename: utils.py
 # prepare some functions that can be used globally, such as read the reference to the memory, read the fastq file to dict
+import subprocess
+import signal  # only used for the exe function
+import time
+from functools import wraps
 
+import os
 
 # The sequence operation functions--------------------------------------------------------------------------------------
 from Bio import SeqIO
-
+from Bio import SeqIO
+import gzip
 
 def fastq2dic(fastqfile):
     """
     Give a fastq file name, return a dict contains the name and seq
     Require Biopython SeqIO medule to parse the sequence into dict, a large readfile may take a lot of RAM
     """
-    handle=open(fastqfile, "rU")
+    if ".gz" in fastqfile:
+        handle=gzip.open(fastqfile, "rU")
+    else:
+        handle=open(fastqfile, "rU")
     record_dict=SeqIO.to_dict(SeqIO.parse(handle, "fastq"))
     handle.close()
     return record_dict
@@ -49,7 +58,7 @@ def chr_select(seq_dict, chro, start,end):
      ("I:100_109","AAAAAAAAAA")
     """
     name=chro+ ":"+str(start)+"_"+str(end)
-    seq=str(seq_dict[chro])[start:end]
+    seq=str(seq_dict[chro][start:end].seq)
     return name,seq
 
 def dic2fasta(record_dict,out="record_dict.fasta"):
@@ -80,15 +89,11 @@ def max_get(listname):
 # ----------------------------------------------------------------------------------------------------------------------
 
 # The system operation functions----------------------------------------------------------------------------------------
-import subprocess
-import signal  # only used for the exe function
-import time
-from functools import wraps
 
 def myexe(cmd, timeout=0):
     """
     a simple wrap of the shell
-    mainly used to run the bwa mem mapping and samtools orders
+    mainly used to run the bwa mem mapping and samtool orders
     """
     def setupAlarm():
         signal.signal(signal.SIGALRM, alarmHandler)
@@ -99,8 +104,8 @@ def myexe(cmd, timeout=0):
 
     proc=subprocess.Popen(cmd, shell=True, preexec_fn=setupAlarm,
                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     out, err=proc.communicate()
+    print err
     return out, err, proc.returncode
 
 
@@ -122,11 +127,38 @@ def timer(fn):
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def bwa_index(fastafile):
-    myexe("bwa index %s" % fastafile)
+def bwa_index(ref):
+    myexe("bwa index %s" % ref)
 
-def bwa_mem(core=15, ):
-    pass
+def bwa_mem(ref, reads, core=15):
+    '''
+    :param core: the core used to run bwa mem
+    :param ref: the full path or relative path to the ref file, need to be in fasta format
+    :param reads: the full path or relative path to the read file, need to be in fastq format
+    :return: None? or sth?
+    '''
+    assert ref.split(".")[-1] in ["fa","fasta"], "Check your reference type, and make them end with .fa or .fasta"
+    assert reads.split(".")[-1] in ["fq","fastq"], "Check your reads type, and make them end with .fq or .fastq"
+
+    ref_short=ref.split("/")[-1].split(".")[0]
+    reads_short=reads.split("/")[-1].split(".")[0]
+    reads_short_s=reads_short+"s"
+
+    # index
+    myexe("bwa index %s" % ref)
+    # bwa to bam file
+    print myexe("bwa mem -t {core} {refpath} {readpath} | samtools view -bS - |samtools sort - {reads_short_s}."
+          .format(core=core, readpath=reads, refpath=ref, reads_short_s=reads_short_s))
+    # sort bam file
+    # myexe("samtools sort {reads_short}.bam {reads_short_s}".
+    #      format(reads_short=reads_short,reads_short_s=reads_short_s))
+    # index bam file
+    myexe("samtools index {reads_short_s}.bam".
+          format(reads_short_s=reads_short_s))
+    # clean up
+    #print myexe("rm {reads_short}.bam".format(reads_short=reads_short))
+
+
 
 ### unit test code ###
 if __name__== "__main__":
